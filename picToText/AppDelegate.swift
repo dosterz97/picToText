a4e845b3b2ee5ea8a4483d61fa6a7c4ef8e649bb
Jack Allen <jackykrabs@gmail.com>
2019-11-08 22:36:05 -0600

    change projy to support multiple views

--- picToText/AppDelegate.swift
+++ picToText/AppDelegate.swift
@@ -15,9 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let mainViewController = UIViewController()
-        mainViewController.view.backgroundColor = UIColor.blue
-        window!.rootViewController = mainViewController
+        let mainViewController = ViewController()
+        let navigationController = UINavigationController.init(rootViewController: mainViewController)
+        window!.rootViewController = navigationController
         window!.makeKeyAndVisible()
         return true
     }
