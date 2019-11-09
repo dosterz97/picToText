67df912b938ba5c8638312a4721c6e17cca461ce
Zach Doster <zach.doster@eventbrite.com>
2019-11-08 21:38:09 -0600

    constraints > story boards

--- picToText/AppDelegate.swift
+++ picToText/AppDelegate.swift
@@ -13,9 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let mainViewController = UIViewController()
+        mainViewController.view.backgroundColor = UIColor.blue
+        window!.rootViewController = mainViewController
+        window!.makeKeyAndVisible()
         return true
     }
 
