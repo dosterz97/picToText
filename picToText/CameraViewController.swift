fda8e5a9d2b5af2be780887de62b152a90afdb07
Jack Allen <jackykrabs@gmail.com>
2019-11-08 23:16:10 -0600

    Added more camera functionality

--- picToText/CameraViewController.swift
+++ picToText/CameraViewController.swift
@@ -12,12 +12,15 @@ import AVFoundation
 class CameraViewController: UIViewController {
 
     var captureSession: AVCaptureSession!
-    
+    var tapRecognizer: UITapGestureRecognizer!
+    var capturePhotoOutput: AVCapturePhotoOutput! // NEW
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupCamera()
-
+        setupTapRecognizer()
+        setupPhotoOutput() // NEW
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -45,4 +48,49 @@ class CameraViewController: UIViewController {
       videoPreviewLayer.frame = view.layer.bounds
       view.layer.addSublayer(videoPreviewLayer)
     }
+    
+    private func setupPhotoOutput() {
+        capturePhotoOutput = AVCapturePhotoOutput()
+        capturePhotoOutput.isHighResolutionCaptureEnabled = true
+        captureSession.addOutput(capturePhotoOutput)
+    }
+    
+    private func setupTapRecognizer() {
+        tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        tapRecognizer?.numberOfTouchesRequired = 1
+        tapRecognizer?.numberOfTouchesRequired = 1
+        view.addGestureRecognizer(tapRecognizer)
+    }
+    
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            capturePhoto()
+        }
+    }
+    
+}
+extension CameraViewController : AVCapturePhotoCaptureDelegate {
+  private func capturePhoto() {
+    let photoSettings = AVCapturePhotoSettings()
+    photoSettings.isAutoStillImageStabilizationEnabled = true
+    photoSettings.isHighResolutionPhotoEnabled = true
+    photoSettings.flashMode = .auto
+    capturePhotoOutput?.capturePhoto(with: photoSettings, delegate: self)
+  }
+
+  func photoOutput(_ output: AVCapturePhotoOutput,
+                   didFinishProcessingPhoto photo: AVCapturePhoto,
+                   error: Error?) {
+    guard error == nil else {
+      fatalError("Failed to capture photo: \(String(describing: error))")
+    }
+    guard let imageData = photo.fileDataRepresentation() else {
+      fatalError("Failed to convert pixel buffer")
+    }
+    guard let image = UIImage(data: imageData) else {
+      fatalError("Failed to convert image data to UIImage")
+    }
+    print(image.size)
+  }
 }
